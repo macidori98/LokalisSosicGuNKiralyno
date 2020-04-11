b4700ba65d0b03906ff3c183d4b317b9ddc517c5
Edwinke <edwinbukkei@gmail.com>
2020-04-11 08:59:36 +0300

    Add files via upload

---
+++ ListExtension.cs
@@ -0,0 +1,82 @@
+﻿namespace kiralyno
+{
+  using System;
+  using System.Collections.Generic;
+
+  public static class ListExtension
+  {
+    public static List<int> InitializeRandomList(this List<int> list, int size)
+    {
+      Random rand = new Random();
+      List<int> numbers = new List<int>();
+      HashSet<int> set = new HashSet<int>();
+
+      while (numbers.Count < size)
+      {
+        int num;
+        do
+        {
+          num = rand.Next(0, size);
+        } while (set.Contains(num));
+        set.Add(num);
+        numbers.Add(num);
+      }
+
+      return numbers;
+    }
+
+    public static void Swap(this List<int> list, int i, int j)
+    {
+      List<int> temp = CopyList(list);
+
+      int v = temp[i];
+      list[i] = list[j];
+      list[j] = v;
+    }
+
+    public static List<int> CopyList(this List<int> list)
+    {
+      List<int> temp = new List<int>();
+
+      for (int i = 0; i < list.Count; ++i)
+      {
+        temp.Add(list[i]);
+      }
+
+      return temp;
+    }
+
+    public static void Print(this List<int> list)
+    {
+      for (int i = 0; i < list.Count; ++i)
+      {
+        Console.Write($"{list[i]} ");
+      }
+
+      Console.WriteLine();
+      Console.WriteLine();
+    }
+
+    public static void PrintBoard(this List<int> list)
+    {
+      int n = list.Count;
+      for (int i = 0; i < n; ++i)
+      {
+        for (int j = 0; j < n; ++j)
+        {
+          if (list[i] != j)
+          {
+            Console.Write(" * ");
+            continue;
+          }
+
+          Console.Write($" {list[i]} ");
+        }
+
+        Console.WriteLine();
+      }
+
+      Console.WriteLine();
+    }
+  }
+}
---
+++ Program.cs
@@ -0,0 +1,120 @@
+﻿namespace kiralyno
+{
+  using System;
+  using System.Collections.Generic;
+
+  public class NonStatic
+  {
+    private int numberOfSwaps, colNo;
+    private List<int> q = new List<int>();
+    private List<int> q2 = new List<int>();
+    private int n;
+    private bool isAttacked = false;
+
+    public void SusicGu(int n)
+    {
+      this.n = n;
+
+      do
+      {
+        q = q.InitializeRandomList(this.n);
+        Console.WriteLine("Init:");
+        this.q.Print();
+        this.q.PrintBoard();
+        this.colNo = this.CollisionCalc(this.q);
+        do
+        {
+          this.numberOfSwaps = 0;
+
+          for (int i = 0; i < this.n; ++i)
+          {
+            for (int j = i + 1; j < this.n; ++j)
+            {
+              int a;
+              bool A = this.IsAttack(out a, i, this.q);
+              bool B = this.IsAttack(out a, j, this.q);
+              if (A || B)
+              {
+                this.q2 = new List<int>();
+
+                for (int k = 0; k < this.n; ++k)
+                {
+                  this.q2.Add(this.q[k]);
+                }
+
+                this.q2.Swap(i, j);
+                int colNo2 = this.CollisionCalc(this.q2);
+                if (this.colNo > colNo2)
+                {
+                  for (int k = 0; k < this.q.Count; ++k)
+                  {
+                    this.q[k] = this.q2[k];
+                  }
+
+                  this.numberOfSwaps = this.numberOfSwaps + 1;
+                  this.colNo = colNo2;
+                }
+              }
+            }
+          }
+        } while (this.numberOfSwaps != 0);
+      } while (this.colNo != 0);
+
+      Console.WriteLine("Found: ");
+      this.q.Print();
+      this.q.PrintBoard();
+    }
+
+    private bool IsAttack(out int aScore, int pos, List<int> table)
+    {
+      int d;//distance
+      aScore = 0;
+      bool a = false;
+
+      for (int i = 0; i < this.q.Count; ++i)
+      {
+        if (i != pos)
+        {
+          d = Math.Abs(pos - i);
+
+          if ((table[i] == table[pos] + d) || (table[i] == table[pos] - d))
+          {
+            a = true;
+            aScore = aScore + 1;
+          }
+        }
+      }
+
+      return a;
+    }
+
+    private int CollisionCalc(List<int> table)
+    {
+      int fAttack = 0;
+
+      List<int> temp = table.CopyList();
+
+      for (int i = 0; i < this.n; ++i)
+      {
+        int a;
+        this.isAttacked = this.IsAttack(out a, i, temp);
+        if (this.isAttacked)
+        {
+          fAttack = fAttack + a;
+        }
+      }
+
+      return fAttack;
+    }
+  }
+
+  internal class Program
+  {
+    private static void Main(string[] args)
+    {
+      NonStatic nonStatic = new NonStatic();
+      nonStatic.SusicGu(5);
+      Console.ReadKey();
+    }
+  }
+}
